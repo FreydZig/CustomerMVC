30670eeccf7078a29426e8894f7a08b2f506b760
FreydZig <zigmunt.freyd.95@gmail.com>
2022-08-31 17:40:44 +0700

    add: customers and address

---
+++ src/CustomerLIb.MVC/Controllers/AddressController.cs
@@ -0,0 +1,96 @@
+﻿using CustomerLIbrary.Entities;
+using CustomerLIbrary.Interfaces;
+using CustomerLIbrary.Repositories;
+using System.Web.Mvc;
+
+namespace CustomerLIb.MVC.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly IRepository<Address> _addressRepository;
+
+        public AddressController()
+        {
+            _addressRepository = new AddresRepository();
+        }
+
+        // GET: Address/5
+        public ActionResult Index(int id)
+        {
+            var address = _addressRepository.GetAll(id.ToString());
+            return View(address);
+        }
+
+        // GET: Address/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        // GET: Address/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Address/Create
+        [HttpPost]
+        public ActionResult Create(Address address)
+        {
+            try
+            {
+                _addressRepository.Create(address);
+                //TODO:FIX
+                return RedirectToAction("Index", address.CustomerId);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Address/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Address/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Address/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Address/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/CustomerLIb.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,108 @@
+﻿using CustomerLIbrary.Entities;
+using CustomerLIbrary.Interfaces;
+using CustomerLIbrary.Repositories;
+using System.Web.Mvc;
+
+namespace CustomerLIb.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IRepository<Customer> _customerRepository;
+
+        public CustomerController()
+        {
+            _customerRepository = new CustomerRepository();
+        }
+
+        public CustomerController(IRepository<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customer = _customerRepository.GetAll("");
+
+            return View(customer);
+        }
+
+        // GET: Customer/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                _customerRepository.Create(customer);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var customer = _customerRepository.Read(id.ToString());
+            return View(customer);
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                _customerRepository.Update(customer);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var customer = _customerRepository.Read(id.ToString());
+            return View(customer);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Customer customer)
+        {
+            try
+            {
+                var _addresRepository = new AddresRepository();
+                var _notesRepository = new NotesRepository();
+                _addresRepository.Delete(id.ToString());
+                _notesRepository.Delete(id.ToString());
+                _customerRepository.Delete(id.ToString());
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
